621eac215b784fa7f7c44a084d05baad4c533cef
arko0220 <artem.kostiuchik@netcracker.com>
2026-07-09 18:15:02 +0500

    fix: add env

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,6 +33,8 @@ source /scripts/email-notification/generate-email-notification-json.sh
 # shellcheck disable=SC1091
 source /scripts/native-report.sh
 # shellcheck disable=SC1091
+source /scripts/generate-bruno-global-env.sh
+# shellcheck disable=SC1091
 source /scripts/render-environment-configuration.sh
 # shellcheck disable=SC1091
 source /scripts/parse-extra-vars.sh
@@ -49,6 +51,10 @@ trap 'finalize_once' EXIT
 init_environment              || fail "Environment initialization failed"
 parse_extra_vars              || fail "EXTRA_VARS parsing failed"
 clone_repository              || fail "Repository clone failed"
+render_environment_configuration || fail "Render Environment Configuration Failed"
+if [ -z "${BRUNO_GLOBAL_ENV}" ]; then
+  generate_bruno_global_env        || fail "Bruno global environment generation failed"
+fi
 setup_runtime_environment     || fail "Runtime setup failed"
 start_upload_monitoring
 run_tests || fail "Test runner failed"
